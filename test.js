867d687db2768728d540e033248812e1633154a1
AndyDaMandy <maldonado.andy.m@gmail.com>
2019-05-20 21:39:59 -0400

    Update test.js

--- test.js
+++ test.js
@@ -50,3 +50,32 @@ function chessBoard(size) {
 
 chessBoard(6);
 
+/* Recursion! I actually figured it out! 
+The reason why you'd use rescursion here is becaue you want to use a function that requires a return statement!
+This is important for checking the absolute value of a negative number before checking if it's even or odd.  */
+
+function isEven(x) {
+  
+  function abCheck (y){
+   return Math.abs(y);
+};
+x = abCheck(x);
+  for (x; x >= 0; x = x - 2) {
+   if (x == 0){
+     
+   return true }
+   
+    if (x == 1) {
+    return false
+  }
+  };
+};
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+console.log(isEven(-25));
+console.log(isEven(-36));
